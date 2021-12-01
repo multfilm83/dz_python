1db872eb6ee267455981af6600f6aad18a05e470
Zverev Andrei <9404147@gmail.com>
2021-12-01 15:43:05 +0300

    dz.py

---
+++ dz_1.py
@@ -0,0 +1,56 @@
+name = "Andy"
+print(name)
+name_type = type(name)
+print(name_type)
+
+name = 38
+print(name)
+name_type = type(name)
+print(name_type)
+
+fl_digit = 32.566
+print(fl_digit)
+name_type = type(fl_digit)
+print(name_type)
+
+b_type = bytes(12)
+print(b_type)
+name_type = type(b_type)
+print(name_type)
+
+list_type = ["123d", "dfee", "123", True, ["12"]]
+print(list_type)
+name_type = type(list_type)
+print(name_type)
+
+tuple_type = (12, "14", "dfr")
+print(tuple_type)
+name_type = type(tuple_type)
+print(name_type)
+
+num_set = {1, 2, 3, 4, 5, 6}
+print(num_set)
+name_type = type(num_set)
+print(name_type)
+
+X = frozenset([1, 2, 3, 4, 5, 6])
+print(X)
+name_type = type(X)
+print(name_type)
+
+dict_Andrey = {"key1": 12, "name_1": "Andrey"}
+print(dict_Andrey["name_1"])
+name_type = type(dict_Andrey)
+print(name_type)
+
+name = "Andrey"
+nAme = "Zverev"
+print(name + nAme)
+
+name = "Andrey"
+age = 38
+print(name, age)
+
+name = "Andrey"
+height = "173"
+print(name + height)
